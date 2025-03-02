83781b5f840fe64d03c81e26afe6eeabc466e701
pavelm0403 <machenii_13@mail.ru>
2025-03-02 15:01:48 +0200

    ...

--- js/sushi.js
+++ js/sushi.js
@@ -13,16 +13,15 @@ function Sushi(x,y,speed,color,size,sushi,slicedSushi1,slicedSushi2,name){
     this.sliced = false;
     this.visible = true;
 }
-
 Sushi.prototype.draw = function(){
-    fill(this.color);
     if(this.sliced && this.name != 'boom'){
         image(this.slicedSushi1, this.x - 25, this.y, this.size, this.size);
         image(this.slicedSushi2, this.x + 25, this.y, this.size, this.size);
     }else{
-        image(this.sushi, this.x, this.y, this.size, this.size);
+        image(this.sushi, this.x, this.y, this.size * (width / 800), this.size * (height / 635));
     }
 };
+
 
 Sushi.prototype.update = function(){
     if(this.sliced && this.name != 'boom'){
